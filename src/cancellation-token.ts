6036fef451b2b23e13c54990e9fa90f7882b1e00
Pavel Hudau <paylikn@gmail.com>
2023-05-12 06:45:55 -0400

    adding ffirst version

---
+++ src/cancellation-token.ts
@@ -0,0 +1,11 @@
+export class CancellationToken {
+  #isCancelled: boolean = false;
+
+  public get isCancelled(): boolean {
+    return this.#isCancelled;
+  }
+
+  public cancel(): void {
+    this.#isCancelled = true;
+  }
+}
