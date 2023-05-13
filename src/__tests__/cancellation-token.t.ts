8805f7c82e9c308fe915ad3b2b6b36bb2b36f8ee
Pavel Hudau <paylikn@gmail.com>
2023-05-13 09:15:04 -0400

    adding tests

---
+++ src/__tests__/cancellation-token.t.ts
@@ -0,0 +1,31 @@
+import { CancellationToken } from "../cancellation-token";
+
+test("CancellationToken is not cancelled when constructed", () => {
+    // GIVEN
+    // WHEN
+    const token = new CancellationToken();
+    //THEN
+    expect(token.isCancelled).toBe(false);
+});
+
+
+test("CancellationToken is cancelled", () => {
+    // GIVEN
+    const token = new CancellationToken();
+    // WHEN
+    token.cancel();
+    //THEN
+    expect(token.isCancelled).toBe(true);
+});
+
+
+test("CancellationToken can be cancelled many times", () => {
+    // GIVEN
+    const token = new CancellationToken();
+    // WHEN
+    for(let i = 0; i < 10; i++) {
+        token.cancel();
+    }
+    //THEN
+    expect(token.isCancelled).toBe(true);
+});
